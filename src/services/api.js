0fd94af364c5ed30f8281cdf5f3c5b9947484a32
Nebil Sherefa <your-real-email@example.com>
2026-07-18 15:15:58 +0300

    update on api.js

--- src/services/api.js
+++ src/services/api.js
@@ -12,7 +12,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'https://neba-backend.onrender.com/api',
+  baseURL: import.meta.env.VITE_API_URL || 'https://ousman-backend.onrender.com/api',
   headers: { 'Content-Type': 'application/json' },
 });
 
